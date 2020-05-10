bff63e18af6a5d1d67b8314cc0a203d223cf8169
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-05-10 16:47:03 +0200

    Update Form1.cs

--- Sprite/Form1.cs
+++ Sprite/Form1.cs
@@ -22,7 +22,7 @@ namespace Sprite
 
             //UPDATER
             WebClient webClient = new WebClient();            
-            if (!webClient.DownloadString("https://pastebin.com/raw/7PZd8stk").Contains("1.0"))
+            if (!webClient.DownloadString("https://pastebin.com/raw/7PZd8stk").Contains("1.1"))
             {
                 if (MessageBox.Show("Update available", "SpriteNDSUpdater", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
